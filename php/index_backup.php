a88e2d91f37b8a966ec8a001148f1bfd9c0588c9
Tomurek <125073033+Tomurek@users.noreply.github.com>
2024-05-20 22:32:15 +0200

    Delete php directory

--- php/index_backup.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-    <form method="post" action="backup.php">
-        Backup<br>
-        <input type="submit" name="Backup" value="Zrób Backup">
-    </form>
-    </body>
-</html>
